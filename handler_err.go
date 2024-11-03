75d46d01af4bf326447f98aafdd82df5662fd82d
Nikita <itsfedos@gmail.com>
2024-11-03 23:40:21 +0300

    error handler and goose first migration

---
+++ handler_err.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"net/http"
+)
+
+func handlerError(w http.ResponseWriter, r *http.Request) {
+	respondWithError(w, http.StatusBadRequest, "Request goes wrong")
+}
--- main.go
+++ main.go
@@ -33,7 +33,8 @@ func main() {
 	}))
 
 	v1Router := chi.NewRouter()
-	v1Router.HandleFunc("/healthz", handlerReadiness)
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerError)
 	router.Mount("/v1", v1Router)
 
 	svr := &http.Server{
