492d9f2f30901268658a56e7bfa60d8c084304e8
Nikita <itsfedos@gmail.com>
2024-11-12 01:26:39 +0300

    prepare goroutines to scrape feeds

--- scraper.go
+++ scraper.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/plyajniq/rss-agg/internal/database"
@@ -16,7 +17,7 @@ func startScraping(
 	log.Printf("Start scrappig with %v goroutines and %s time between requests", concurrency, timeBetweenRequets)
 	ticker := time.NewTicker(timeBetweenRequets)
 	for ; ; <-ticker.C {
-		feeds, err :=db.GetNextFeedsToFetch(
+		feeds, err := db.GetNextFeedsToFetch(
 			context.Background(),
 			int32(concurrency),
 		)
@@ -24,5 +25,21 @@ func startScraping(
 			log.Printf("error to fetching: %v", err)
 			continue
 		}
+
+		wg := &sync.WaitGroup{}
+		// inter feeds in main goroutine
+		for _, feed := range feeds {
+			// +1 to counter of goroutines to wait
+			wg.Add(1)
+			// spawn goroutine
+			go scrapeFeed(db, wg, feed)
+		}
+		// wait for all to finish
+		wg.Wait()
 	}
+}
+
+func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
+	// -1 to counter of goroutines to wait
+	defer wg.Done()
 }
